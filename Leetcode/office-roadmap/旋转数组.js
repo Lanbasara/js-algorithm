9999e658f463914b5059ad2e46a46dc1c2ac8410
Lanbasara <ghkagg@outlook.com>
2021-08-17 13:10:24 +0000

    旋转数组

---
+++ Leetcode/office-roadmap/旋转数组.js
@@ -0,0 +1,34 @@
+/**
+ * 旋转数组
+ * 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+ *
+ * 需要原地算法，空间复杂度为O(1)
+ *  
+ * 输入: nums = [1,2,3,4,5,6,7], k = 3
+ * 输入: nums = [1,2,3,4,5,6,7], k = 3
+ * 输出: [5,6,7,1,2,3,4]
+ * 解释:
+ * 向右旋转 1 步: [7,1,2,3,4,5,6]
+ * 向右旋转 2 步: [6,7,1,2,3,4,5]
+ * 向右旋转 3 步: [5,6,7,1,2,3,4]
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+ var rotate = function(nums, k) {
+  if(k === 0) return
+  k = k%nums.length
+  function reverse(nums,start,end){
+    while(start < end){
+      [nums[start],nums[end]] = [nums[end],nums[start]]
+      start++;
+      end--;
+    }
+  }
+  reverse(nums,0,nums.length-1)
+  reverse(nums,0,k-1)
+  reverse(nums,k,nums.length-1)
+};
