f53bd43ef4b12a2bdbb857d8808bf8e288591c13
Lanbasara <ghkagg@outlook.com>
2021-09-06 14:24:54 +0000

    洗牌算法

---
+++ algorithm/shuffler.js
@@ -0,0 +1,9 @@
+function shuffler(array){
+  for(let i=0;i<array.length;i++){
+    let index = Math.floor(Math.random() * (i+1));
+    [array[i],array[index]] = [array[index],array[i]]
+  }
+  return array
+}
+
+console.log(shuffler([1,2,3,4,5,6]))
