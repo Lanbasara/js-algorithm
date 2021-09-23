d0862e52f3c85ae3dee96ff8fdc462dfc41cd957
Lanbasara <ghkagg@outlook.com>
2021-09-23 04:41:18 +0000

    归并排序

--- algorithm/merge-sort.js
+++ algorithm/merge-sort.js
@@ -3,10 +3,33 @@
  * 以数组为例子
  */
 function mergeSort(array) {
-  function merge()
-  function sort(target, start, end) {
-    let mid = Math.floor(left + (right - left) / 2);
-    
+  function sort(array, start, end) {
+    if (start === end) return;
+    let mid = Math.floor(start + (end - start) / 2);
+    sort(array, start, mid);
+    sort(array, mid + 1, end);
+    let i = 0;
+    let p1 = start;
+    let p2 = mid + 1;
+    let help = [];
+    while (p1 <= mid && p2 <= end) {
+      help[i++] = array[p1] < array[p2] ? array[p1++] : array[p2++];
+    }
+    while (p1 <= mid) {
+      help[i++] = array[p1++];
+    }
+    while (p2 <= end) {
+      help[i++] = array[p2++];
+    }
+    for (let i = 0; i < help.length; i++) {
+      array[start + i] = help[i];
+    }
+    return array;
   }
   sort(array, 0, array.length - 1);
+  return array;
 }
+
+let a = [3, 2, 6, 5, 7, 1, 0, -10];
+console.log(mergeSort(a));
+console.log(a);
