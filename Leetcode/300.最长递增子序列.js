17e6a8a48d6b6566cce6e4bdc20181a1afd3e2d3
Lanbasara <hg1483@nyu.edu>
2022-04-13 13:58:06 +0800

    300

---
+++ Leetcode/300.最长递增子序列.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=300 lang=javascript
+ *
+ * [300] 最长递增子序列
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function (nums) {
+  // dp[i], 以i为结尾的元素, 最长的递增子序列长度
+  let dp = new Array(nums.length).fill(1);
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+  }
+
+  return Math.max(...dp);
+};
+console.log(lengthOfLIS([0, 1, 0, 3, 2, 3]));
+// @lc code=end
