acf4479b4d806ca3532b01112f54bf1e36a5a923
Lanbasara <ghkagg@outlook.com>
2023-02-02 10:21:07 +0800

    segement-tree

--- Test/data-structure/segment-tree.test.js
+++ Test/data-structure/segment-tree.test.js
@@ -1,12 +1,7 @@
 const { SegmentTree } = require('../../data-structure/segment-tree');
 
 describe('Segment-tree test', () => {
-  it('1. Build tree', () => {
-    const tree = new SegmentTree([10, 11, 12, 13, 14]);
-    expect(tree.tree).toEqual([60, 33, 27, 21, 12, 13, 14, 10, 11]);
-  });
-
-  it('2. sumRange', () => {
+  it('1. sumRange', () => {
     const tree = new SegmentTree([10, 11, 12, 13, 14]);
     expect(tree.sumRange(2, 4)).toBe(39);
     expect(tree.sumRange(0, 0)).toBe(10);
